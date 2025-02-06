8102cbfed540e58a5714500cf08c4207a62c2e87
Yorokobi <69779226+Non2559TH@users.noreply.github.com>
2025-02-06 12:13:46 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -35,28 +35,29 @@ def recommend(song):
         recommended_music_posters.append(get_song_album_cover_url(music.iloc[i[0]].song, artist))
         recommended_music_names.append(music.iloc[i[0]].song)
 
-    return recommended_music_names,recommended_music_posters
+    return recommended_music_names, recommended_music_posters
 
 st.header('Music Recommender System')
-music = pickle.load(open('df.pkl','rb'))
-similarity = pickle.load(open('similarity.pkl','rb'))
+
+# Update the path to your files
+music = pickle.load(open('C:/JN/data/df.pkl', 'rb'))
+similarity = pickle.load(open('C:/JN/data/similarity.pkl', 'rb'))
 
 music_list = music['song'].values
-selected_movie = st.selectbox(
+selected_song = st.selectbox(
     "Type or select a song from the dropdown",
     music_list
 )
 
 if st.button('Show Recommendation'):
-    recommended_music_names,recommended_music_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5= st.columns(5)
+    recommended_music_names, recommended_music_posters = recommend(selected_song)
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(recommended_music_names[0])
         st.image(recommended_music_posters[0])
     with col2:
         st.text(recommended_music_names[1])
         st.image(recommended_music_posters[1])
-
     with col3:
         st.text(recommended_music_names[2])
         st.image(recommended_music_posters[2])
@@ -66,7 +67,3 @@ if st.button('Show Recommendation'):
     with col5:
         st.text(recommended_music_names[4])
         st.image(recommended_music_posters[4])
-
-
-
-
